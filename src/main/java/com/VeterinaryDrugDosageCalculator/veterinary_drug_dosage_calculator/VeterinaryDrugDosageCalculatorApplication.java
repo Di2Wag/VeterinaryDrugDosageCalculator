3d4f922d9225f5b97ec7992372326218d43286a3
Di2Wag <diana.margarida@gmail.com>
2025-07-11 22:45:59 +0100

    build project structure

---
+++ src/main/java/com/VeterinaryDrugDosageCalculator/veterinary_drug_dosage_calculator/VeterinaryDrugDosageCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.VeterinaryDrugDosageCalculator.veterinary_drug_dosage_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryDrugDosageCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryDrugDosageCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VeterinaryDrugDosageCalculator/veterinary_drug_dosage_calculator/VeterinaryDrugDosageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VeterinaryDrugDosageCalculator.veterinary_drug_dosage_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryDrugDosageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
